4aac5794f68ac8c59804cd6bb39302df0b81316b
greyland miller <greyland.official@gmail.com>
2024-04-28 00:07:32 -0700

    color change

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -7,7 +7,7 @@ return {
     -- or leave it empty to use the default settings
     style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style = "day", -- The theme is used when the background is set to light
-    transparent = false, -- Enable this to disable setting the background color
+    transparent = true, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
     styles = {
       -- Style to be applied to different syntax groups
@@ -15,7 +15,7 @@ return {
       comments = { italic = true, fg = "#567494" },
       keywords = { italic = true },
       functions = {},
-      variables = { fg = "#f2578d" },
+      variables = { fg = "#bdecf2" },
       -- Background styles. Can be "dark", "transparent" or "normal"
       sidebars = "dark", -- style for sidebars, see below
       floats = "dark", -- style for floating windows
